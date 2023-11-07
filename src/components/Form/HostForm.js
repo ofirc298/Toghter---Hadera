dd6a87fca70b66d53030b69c878cb29ec61335ad
ofirc298 <61381462+ofirc298@users.noreply.github.com>
2023-11-07 11:13:20 +0200

    contacy

--- src/components/Form/HostForm.js
+++ src/components/Form/HostForm.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import arrow from '../../assets/arrow.svg'
 function HostForm() {
   return (
-    <div>HostForm</div>
+    <div>
+        <div className="w-[529px] text-center text-black text-2xl font-normal font-['Heebo']">נשמח למילוי השאלון וניצור אתכם קשר בהקדם</div>
+        <img src={arrow} alt='arrow' />
+
+
+    </div>
   )
 }
 
