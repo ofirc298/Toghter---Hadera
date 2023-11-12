167af8926a4a6cea2f9d2756eb5f09ac0be1c92f
ofirc298 <61381462+ofirc298@users.noreply.github.com>
2023-11-12 18:56:15 +0200

    host

--- src/components/ApiUtils.js
+++ src/components/ApiUtils.js
@@ -5,7 +5,7 @@ export async function postData(details) {
     try {
         if (details.familyType === "host") {
             console.log(details);
-            const postedData = await axios.post("https://togther-hadera-server.onrender.com/visits", details) //API address should be set here
+            const postedData = await axios.post("https://togther-hadera-server.onrender.com/hosts", details) //API address should be set here
             return { MSG: "The data has been sent successfully", DATA: postedData };
         } else if (details.familyType === "guest") {
             console.log(details);
